0c3c9a999892d84f5a7683c4ad300445789f263b
shubhamkhadke <shubham432@gmail.com>
2025-01-25 13:48:58 +0530

    commit 443

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
 <<<<<<< HEAD
  res.send('hello');
 =======
- res.send('Welcome to Fortune Cloud Tchnologies');
+ res.send('Welcome to Fortune Cloud Tchnologies!!!!');
 >>>>>>> 3e0f59702d0a335fe1b2e12dfd792974e32b3df8
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
