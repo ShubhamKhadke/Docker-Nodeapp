f3adaeb684cc2b7feb9064aeed03dfbf9e8a2e22
shubhamkhadke <shubham432@gmail.com>
2025-01-28 11:31:43 +0530

    commit aaj ka first

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('just another trail');
+ res.send('dekhta hu chalu hai kya nahi');
 
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
